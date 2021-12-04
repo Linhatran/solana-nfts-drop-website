e6a90dd72925fad0a51c6e2ead01d9610e5af028
Linh Tran <linhtanl51@gmail.com>
2021-12-04 12:20:53 -0800

    check if Phantom wallet is connected

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,21 +7,28 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const checkIfPhantomIsConnected = () => {
+    const { solana } = window;
+    
+    if (solana.isPhantom) {
+      console.log('Connected to Phantom wallet');
+    } else {
+      console.log(
+        'Phantom wallet not found. Please connect to a Phantom wallet'
+      );
+    }
+  };
+
+  useEffect(() => {
+    checkIfPhantomIsConnected();
+  }, []);
+
   return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+    <div className='App'>
+      <div className='container'>
+        <div className='header-container'>
+          <p className='header'>🍭 Candy Drop</p>
+          <p className='sub-text'>NFT drop machine with fair mint</p>
         </div>
       </div>
     </div>
